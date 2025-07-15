aa41b9259831b12c685d58e3910d0087a5b29808
VP <squid_201@hotmail.com>
2025-07-14 22:03:09 -0400

    Week-02: added Form component

---
+++ src/TodoForm.jsx
@@ -0,0 +1,13 @@
+
+
+function TodoForm() {
+    return(
+        <form>
+            <label htmlFor="todoTitle">Todo</label>
+            <input id="todoTitle" type='text'></input>
+            <button>Add Todo</button>
+        </form>
+    )
+}
+
+export default TodoForm;
